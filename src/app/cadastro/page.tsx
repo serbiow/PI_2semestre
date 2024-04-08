8e3089d1fcf07de39dd502585c229aafbce89c48
Daniel Modesto <142944953+DaniMitr@users.noreply.github.com>
2024-04-08 18:58:14 -0300

    Add files via upload

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -25,9 +25,25 @@ export default function cadastro(){
     className="grow bg-slate-100" 
     placeholder="Email" />
 </label>
+{/* CPF */} 
+<label className="input input-bordered flex items-center gap-2 mt-2">
+<svg className="w-4 h-4 opacity-70" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 3v4a1 1 0 0 1-1 1H5m4 8h6m-6-4h6m4-8v16a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V7.914a1 1 0 0 1 .293-.707l3.914-3.914A1 1 0 0 1 9.914 3H18a1 1 0 0 1 1 1Z"/>
+</svg>
+
+  <input
+    type="text"
+    className="grow bg-slate-100" 
+    placeholder="CPF" />
+</label>
+
 {/* Telefone */}
 <label className="input input-bordered flex items-center gap-2 mt-2">
-<svg aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="1em" height="1em"><path d="M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 13.9l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C3.9 366.5-2 378.1.6 389.4l24 104C27.1 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-20.9-18.6-23.4z"/></svg> 
+<svg className="w-4 h-4 opacity-70" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.427 14.768 17.2 13.542a1.733 1.733 0 0 0-2.45 0l-.613.613a1.732 1.732 0 0 1-2.45 0l-1.838-1.84a1.735 1.735 0 0 1 0-2.452l.612-.613a1.735 1.735 0 0 0 0-2.452L9.237 5.572a1.6 1.6 0 0 0-2.45 0c-3.223 3.2-1.702 6.896 1.519 10.117 3.22 3.221 6.914 4.745 10.12 1.535a1.601 1.601 0 0 0 0-2.456Z"/>
+</svg>
+
+
 <input type="text" className="grow" placeholder="Telefone" />
 </label>
 {/* Senha */}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
 import Image from "next/image";
 
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
+    <main className="p-0 h-screen w-screen m-0">
+     <nav className=" flex list-none w-screen bg-[#010E21]  text-[#E7C7A1] pt-8 py-8  gap-5">
+    <li> <Image
+        className="ml-10"
+        src=""
+        width={30}
+        height={30}
+        alt="Tail_logo"
+      /></li>
+    <div className="flex list-none w-screen justify-center gap-5">
+    <li className="hover:text-[#f1dabd] transition-all">Cursos</li>
+    <li className="hover:text-[#f1dabd] transition-all">Quem sou eu?</li>
+    <li className="hover:text-[#f1dabd] transition-all">Serviços</li>
+    </div>
+    <div className="text-[#E7C7A1] ml-auto mr-10 hover:text-[#f1dabd] transition-all border-b-[#E7C7A1] ">Login</div>
+     </nav>
       
-      <a href="login"><h1>Tela de login</h1></a>
+     
     </main>
   );
 }
