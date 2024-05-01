caf79684509d5c304446d830ae57f09396a95e3f
Pablo Fernandes Sena <pabupablo2000@gmail.com>
2024-05-01 19:50:42 -0300

    commit 01/05
    Adição da pagina cursos

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        primary:{
+          '100': '#E7C7A1',
+          '200': '#09172C',
+          '300': '#132744',
+          
+          
+        }
+      }
     },
   },
   plugins: [],
